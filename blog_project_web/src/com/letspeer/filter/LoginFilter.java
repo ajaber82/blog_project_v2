ce6841b08d742f4c08fba6ba5f0305e5277db9d2
alaa.abuzaghleh@gmail.com <alaaabuzaghleh@192.168.0.5>
2017-11-24 03:03:19 -0600

    add filter

---
+++ blog_project_web/src/com/letspeer/filter/LoginFilter.java
@@ -0,0 +1,57 @@
+package com.letspeer.filter;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet Filter implementation class LoginFilter
+ */
+@WebFilter("/users")
+public class LoginFilter implements Filter {
+
+    /**
+     * Default constructor. 
+     */
+    public LoginFilter() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		// TODO Auto-generated method stub
+		// place your code here
+
+		// pass the request along the filter chain
+		HttpServletRequest servletRequest = (HttpServletRequest) request ; 
+		HttpServletResponse servletResponse = (HttpServletResponse) response ; 
+		if(servletRequest.getSession().getAttribute("user")==null) {
+			servletResponse.sendRedirect("http://www.google.com");
+		}
+		chain.doFilter(request, response);
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+}
