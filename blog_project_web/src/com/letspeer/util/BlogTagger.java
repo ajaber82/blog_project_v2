901d052c8e68563462bc359ccdf9b423bc18d667
alaa.abuzaghleh@gmail.com <alaaabuzaghleh@Alaas-MacBook-Pro.local>
2017-11-26 06:01:40 -0600

    add classification functionality

--- blog_project_web/src/com/letspeer/util/BlogTagger.java
+++ blog_project_web/src/com/letspeer/util/BlogTagger.java
@@ -10,8 +10,7 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
-import java.util.SortedSet;
-import java.util.TreeSet;
+
 
 public class BlogTagger {
 
